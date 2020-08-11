1f628fd80640505834d4c29680a9bc8aa542c827
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-11 09:33:54 +0630

    create session user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+    has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
